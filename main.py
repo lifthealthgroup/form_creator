52d0d8500e5e05cc1d1801b7ee2371a397fb527d
Frank <frank.snelling03@gmail.com>
2024-10-12 16:50:50 +0100

    BBS, LEFS added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import fitz
 from flask import Flask, request, send_file, render_template
 import os
+import math
 
 app = Flask(__name__)
 
@@ -29,10 +30,12 @@ def read_excel(excel:str):
 
     df = pd.read_excel(excel) # read excel into pandas df
     master = {} # initialise master dictionary
+    
     for col in df.columns:
         if not pd.isna(df[col].iloc[0]) and 'values' in col.lower(): # find dictionaries to populate
             dict_name = col.replace(' Values', '') # dictionary key name
             master[dict_name] = pd.Series(df[col].values, index=df[dict_name]).to_dict() # place dictionary in dictionary
+            master[dict_name] = {k:int(v) if isinstance(v, float) and not math.isnan(v) else v for k, v in master[dict_name].items()} # convert values to integers where possible 
     return master # which contains all info needed to fill forms
 
 def fill_textboxes(general_values:dict, form_values:dict, template):
@@ -48,9 +51,12 @@ def fill_textboxes(general_values:dict, form_values:dict, template):
             if key in general_values: # add general values
                 field.field_value = str(general_values[key])
                 field.update()
-            if key == 'Y1':
-                field.field_value = True 
-                field.update()
+            try: # in case form_values has float valued keys (for LSP)
+                if float(key) in form_values:
+                    field.field_value = str(int(form_values[float(key)]))
+                    field.update()
+            except Exception:
+                pass
     return template
 
 def highlight_box(x0, y0, x1, y1, page):
@@ -84,8 +90,8 @@ def fill_WHODAS(general_values:dict, form_values:dict):
     Inserts values from dictionary in correct fields in WHODAS pdf file.
     """
     template = fitz.open('forms/WHODAS.pdf')
-
-    # Calculate Extra Fields
+            
+    # calculate extra fields
     for i in range(1,7): 
         if i != 5:
             number_params = sum(1 for key, _ in form_values.items() if key.startswith('D' + str(i)))
@@ -95,7 +101,18 @@ def fill_WHODAS(general_values:dict, form_values:dict):
     form_values['5_avg'] = round(form_values['5_overall'] / 4, 2)
     form_values['5_overall2'] = form_values['D55'] + form_values['D56'] + form_values['D57'] + form_values['D58']
     form_values['5_avg2'] = round(form_values['5_overall2'] / 4, 2)
-    form_values['total'] = form_values['1_overall'] + form_values['2_overall'] + form_values['3_overall'] + form_values['4_overall'] + form_values['5_overall'] + form_values['5_overall2'] + form_values['6_overall']
+    
+    # if section 2 of 5 is N/A and left empty
+    if math.isnan(form_values['5_overall2']):
+        form_values['D55'], form_values['D56'], form_values['D57'], form_values['D58'], form_values['5_avg2'], form_values['5_overall2'] = "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+        page = template.load_page(1)
+        page.draw_line((26, 363), (583.7, 209.3), width=2)
+        
+        # find total values
+        form_values['total'] = form_values['1_overall'] + form_values['2_overall'] + form_values['3_overall'] + form_values['4_overall'] + form_values['5_overall'] + form_values['6_overall']
+    else:
+        form_values['total'] = form_values['1_overall'] + form_values['2_overall'] + form_values['3_overall'] + form_values['4_overall'] + form_values['5_overall'] + form_values['5_overall2'] + form_values['6_overall']
+        
     form_values['avg'] = round(form_values['total'] / 36, 2)
     
     # Fill in textboxes
@@ -121,6 +138,7 @@ def fill_CANS(general_values:dict, form_values:dict):
     
     template = fitz.open('forms/CANS.pdf') # read in template pdf
 
+    # add in totals to dictionary
     form_values['A_subtotal'] = 0
     form_values['B_subtotal'] = 0
     form_values['C_subtotal'] = 0
@@ -141,26 +159,44 @@ def fill_CANS(general_values:dict, form_values:dict):
 
     form_values['subtotal'] = form_values['A_subtotal'] + form_values['B_subtotal'] + form_values['C_subtotal'] + form_values['D_subtotal']
 
+    # define coordinates for highlighting
+    x_desc = [638.5,815.8] # for descriptions on right of page
+    y_desc = [121.7,132.5,155.5,178.6,235.4,258.5,281.5,304.6,360.7]
+    
+    page = template.load_page(0) # higlights on page 1
+    
     # calculate CANS level
     if form_values['A_subtotal'] < 4:
         if form_values['B_subtotal'] >= 4:
             form_values['total'] = 4.2
+            page = highlight_box(x_desc[0], y_desc[3], x_desc[1], y_desc[4], page)
         elif form_values['C_subtotal'] >= 4:
             form_values['total'] = 4.1
+            page = highlight_box(x_desc[0], y_desc[3], x_desc[1], y_desc[4], page)
         elif form_values['C_subtotal'] == 3:
             form_values['total'] = 3
+            page = highlight_box(x_desc[0], y_desc[4], x_desc[1], y_desc[5], page)
         elif form_values['C_subtotal'] == 2:
-            form_values['total'] = 2    
+            form_values['total'] = 2 
+            page = highlight_box(x_desc[0], y_desc[5], x_desc[1], y_desc[6], page)
         elif form_values['C_subtotal'] == 1:
             form_values['total'] = 1
+            page = highlight_box(x_desc[0], y_desc[6], x_desc[1], y_desc[7], page)
         else:
-            form_values['total'] = 0    
+            form_values['total'] = 0  
+            page = highlight_box(x_desc[0], y_desc[7], x_desc[1], y_desc[8], page)  
+    elif form_values['A_subtotal'] == 4:
+        form_values['total'] = 4.3
+        page = highlight_box(x_desc[0], y_desc[3], x_desc[1], y_desc[4], page)
     elif form_values['A_subtotal'] == 5:
         form_values['total'] = 5
+        page = highlight_box(x_desc[0], y_desc[2], x_desc[1], y_desc[3], page)
     elif form_values['A_subtotal'] == 6:
         form_values['total'] = 6
+        page = highlight_box(x_desc[0], y_desc[1], x_desc[1], y_desc[2], page)
     else:
         form_values['total'] = 7
+        page = highlight_box(x_desc[0], y_desc[0], x_desc[1], y_desc[1], page)
     
     template = fill_textboxes(general_values, form_values, template) # fill out textboxes
 
@@ -203,19 +239,20 @@ def fill_LSP(general_values:dict, form_values:dict):
             page = highlight_box(x[3] + 10, y[i], x[4] - 10, y[i + 1], page) # bring in the highlight slightly due to formatting
     
     # perform scoring
-    form_values['a_score'] = int(form_values[1] + form_values[2] + form_values[3] + form_values[8])
-    form_values['b_score'] = int(form_values[4] + form_values[5] + form_values[6] + form_values[9] + form_values[16])
-    form_values['c_score'] = int(form_values[10] + form_values[11] + form_values[12])
-    form_values['d_score'] = int(form_values[7] + form_values[13] + form_values[14] + form_values[15])
+    form_values['a_score'] = form_values[1] + form_values[2] + form_values[3] + form_values[8]
+    form_values['b_score'] = form_values[4] + form_values[5] + form_values[6] + form_values[9] + form_values[16]
+    form_values['c_score'] = form_values[10] + form_values[11] + form_values[12]
+    form_values['d_score'] = form_values[7] + form_values[13] + form_values[14] + form_values[15]
     
-    total = form_values['a_score'] + form_values['b_score'] + form_values['c_score'] + form_values['d_score']
-    form_values['total'] = "Total Score:  " + str(total) + "/80 = " + str(total * 1.25) + "/100"
+    form_values['total'] = form_values['a_score'] + form_values['b_score'] + form_values['c_score'] + form_values['d_score']
+    form_values['total_100'] = str(form_values['total'] * 1.25) + "/100"
     
     form_values['a_score'] = str(form_values['a_score']) + "/12"
     form_values['b_score'] = str(form_values['b_score']) + "/15"
     form_values['c_score'] = str(form_values['c_score']) + "/9" 
     form_values['d_score'] = str(form_values['d_score']) + "/12"
     
+    # fill form
     template = fill_textboxes(general_values, form_values, template) # fill out textboxes
 
     return template
@@ -225,6 +262,7 @@ def fill_LAWTON(general_values:dict, form_values:dict):
     Inserts values from dictionary in correct fields in LAWTON pdf file.
     """
     template = fitz.open('forms/LAWTON.pdf') # read in template pdf
+    
     with open('lawton.txt', 'r') as file:     
         sections = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
         i = 0 # counter
@@ -233,14 +271,90 @@ def fill_LAWTON(general_values:dict, form_values:dict):
             template = highlight_text(options[int(form_values[sections[i]]) - 1], template) # highlight relevant number for each column
             i += 1 # increment
     
-    form_values['left_total'] = form_values['A'] + form_values['B'] + form_values['C'] + form_values['D']
-    form_values['right_total'] = form_values['E'] + form_values['F'] + form_values['G'] + form_values['H']
+    # calculate left side total
+    form_values['left_total'] = 0
+    if form_values['A'] != 4:
+        form_values['left_total'] += 1
+    if form_values['B'] == 1:
+        form_values['left_total'] += 1
+    if form_values['C'] == 1:
+        form_values['left_total'] += 1
+    if form_values['D'] != 5:
+        form_values['left_total'] += 1
+        
+    # calcualte right side total
+    form_values['right_total'] = 0
+    if form_values['E'] != 3:
+        form_values['right_total'] += 1
+    if form_values['F'] <= 3:
+        form_values['right_total'] += 1
+    if form_values['G'] == 1:
+        form_values['right_total'] += 1
+    if form_values['H'] != 3:
+        form_values['right_total'] += 1 
+           
+    # calculate total 
     form_values['total'] = form_values['left_total'] + form_values['right_total']
     
     template = fill_textboxes(general_values, form_values, template)
     
     return template
         
+def fill_BBS(general_values:dict, form_values:dict):
+    """
+    Inserts values from dictionary in correct fields in LAWTON pdf file.
+    """
+    template = fitz.open('forms/BBS.pdf') # read in template pdf
+
+    total = 0
+    
+    for page in template: # gather fields
+        for field in page.widgets():
+            key = field.field_name
+            # Check if it's a checkbox
+            if field.field_type == fitz.PDF_WIDGET_TYPE_CHECKBOX:
+                category, value = key.split('_') # gain values for dictionary
+                if form_values[int(category)] == float(value):
+                    total += int(value)
+                    field.field_value = True # mark correct checkboxes
+                    field.update()
+    
+    new_dict = {}
+    new_dict['total'] = total # new dictionary for efficiency, don't search through checkboxes
+    template = fill_textboxes({}, new_dict, template) # no general values on BBS form
+    
+    return template
+
+def fill_LEFS(general_values:dict, form_values:dict):
+    """
+    Inserts values from dictionary in correct fields in LEFS pdf file.
+    """
+    template = fitz.open('forms/LEFS.pdf') # read in template pdf
+    
+    x = [394,407,476,492,546,562,614,628,681,694]
+    y = [199,211,223,235.5,249,261,274,286.5,299,312,324,337,349,362,374,386,399,412.5,425,438,449]
+
+    # highlight correct box for each row
+    page = template.load_page(0)
+    pw = page.rect.width # page width
+    total = 0 # track total value
+    for i in range(20):
+        score = form_values[i + 1]
+        if score == 0:
+            page = highlight_box(y[i] + 2, pw - x[1], y[i + 1] - 2, pw - x[0], page) # bring in the highlight slightly due to formatting
+        elif score == 1:
+            page = highlight_box(y[i] + 2, pw - x[3], y[i + 1] - 2, pw - x[2], page) # bring in the highlight slightly due to formatting
+        elif score == 2:
+            page = highlight_box(y[i] + 2, pw - x[5], y[i + 1] - 2, pw - x[4], page) # bring in the highlight slightly due to formatting
+        elif score == 3:
+            page = highlight_box(y[i] + 2, pw - x[7], y[i + 1] - 2, pw - x[6], page) # bring in the highlight slightly due to formatting
+        total += score
+    general_values['total'] = total # save in general to avoid passing form_values for efficiency
+    
+    template = fill_textboxes(general_values, {}, template) # fill other values
+    
+    return template
+
 def produce_output(master:dict[dict]):
     """
     Calls fill_form function for each dictionary in dictionaries and combines pdfs to final file. 
@@ -272,7 +386,7 @@ def upload_file():
     final_document = produce_output(master)
     
     # Save the output PDF
-    output_path = 'output/new.pdf'
+    output_path = 'output/forms.pdf'
     if not os.path.exists('output'):
         os.makedirs('output')
     
